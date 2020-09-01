aee1e9e93457cd0bb9ae0263001fabbe4ff48530
lucswart <66739939+lucswart@users.noreply.github.com>
2020-09-01 13:56:48 +0200

    Hoofdpagina
    Hoofdpagina bijna af

--- app.js
+++ app.js
@@ -32,6 +32,66 @@ menuBtn.addEventListener('click', () => {
 
 })
 
+//Intro animatie
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = ["snelle", "overzichtelijke", "gemakkelijke", "mooie"];
+const typingDelay = 100;
+const erasingDelay = 100;
+const newTextDelay = 1000; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+  	setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+	if (charIndex > 0) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if(textArrayIndex>=textArray.length) textArrayIndex=0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+  if(textArray.length) setTimeout(type, newTextDelay + 250);
+});
+
+// Woorden swapper intro (jQuery)
+var words = ["websites.","webapplicaties.", "apps."];
+var i = 0;
+var text = "websites.";
+function _getChangedText() {
+    i = (i + 1) % words.length;
+    console.log(words[i]);
+    console.log(i);
+    return text.replace(/websites./, words[i]);
+}
+function _changeText() {
+    var txt = _getChangedText();
+    console.log(txt);
+    document.getElementById("changer").innerHTML = txt;
+}
+setInterval("_changeText()", 5000);
+
 //Alle functies bij een
 const app = () => {
     navSlide();
