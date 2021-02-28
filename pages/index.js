48269a17533959e62b45dedd7ba54598da0ec36f
lucswart <66739939+lucswart@users.noreply.github.com>
2021-02-28 20:34:54 +0100

    Added script

--- pages/index.js
+++ pages/index.js
@@ -163,6 +163,7 @@ export default function Home() {
           href="/favicon-16x16.png"
         />
         <meta name="twitter:card" content="summary_large_image" />
+        <script async src="https://cdn.splitbee.io/sb.js"></script>
       </Head>
       <header className="hero container-fluid">
         <Nav />
